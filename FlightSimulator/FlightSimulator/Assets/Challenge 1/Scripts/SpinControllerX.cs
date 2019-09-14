475438d80d5c8b5b7b9e0ea633ead150050c19dc
Pavan Josyula <chintupawan@gmail.com>
2019-09-14 19:19:35 +1200

    Completed challenge

---
+++ FlightSimulator/FlightSimulator/Assets/Challenge 1/Scripts/SpinControllerX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinControllerX : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Propellor;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Propellor.transform.Rotate(new Vector3 (0, 0, 20) * Time.deltaTime * 15);
+    }
+}
