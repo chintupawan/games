378ead73df0a32d57bf7e2731891e1da79b15120
Pavan Josyula <chintupawan@gmail.com>
2019-09-18 20:50:01 +1200

    Throw for life

---
+++ ThrowForLife/ThrowForLife/Assets/Scripts/Destroyer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float topBound = 30f;
+    private float bottomBound = -10f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.transform.position.z < -10 || gameObject.transform.position.z > 30f){
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ ThrowForLife/ThrowForLife/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandom", 0.5f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    void SpawnRandom()
+    {
+        Vector3 pos =  new Vector3(Random.Range(-20,20), 0, 20);
+        int index = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[index], pos, animalPrefabs[0].transform.rotation);
+    }
+}
